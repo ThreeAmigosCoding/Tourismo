04a7a2c3962913598a8bddad3a1cc77dd843816f
cuturic01 <miloscuturic8@gmail.com>
2023-05-26 13:01:21 +0200

    #6 Implemented RegistrationViewModel
    Additionally, refactored the code so that all the colors, fonts and styles are in separate files.

---
+++ Tourismo/GUI/Auth/RegistrationViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Tourismo.GUI.Utility;
+
+namespace Tourismo.GUI.Auth
+{
+    public class RegistrationViewModel : ViewModelBase
+    {
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+
+        private string _phone;
+        public string Phone
+        {
+            get => _phone;
+            set
+            {
+                _phone = value;
+                OnPropertyChanged(nameof(Phone));
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        private string _errMsgText;
+        public string ErrMsgText
+        {
+            get => _errMsgText;
+            set
+            {
+                _errMsgText = value;
+                OnPropertyChanged(nameof(ErrMsgText));
+            }
+        }
+
+        private Visibility _errMsgVisibility;
+        public Visibility ErrMsgVisibility
+        {
+            get => _errMsgVisibility;
+            set
+            {
+                _errMsgVisibility = value;
+                OnPropertyChanged(nameof(ErrMsgVisibility));
+            }
+        }
+
+        public RegistrationViewModel()
+        {
+            _errMsgVisibility = Visibility.Hidden;
+            _errMsgText = "";
+        }
+
+    }
+}
