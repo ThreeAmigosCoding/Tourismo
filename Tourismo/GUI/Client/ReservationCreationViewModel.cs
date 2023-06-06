cdda74103712dae69f191ca6e5d93e58fedf0fc9
Marko Janosevic <markojanosevicsm7@gmail.com>
2023-06-06 18:01:11 +0200

    #22 Reservation window open
    Window for reservation can now be opened

---
+++ Tourismo/GUI/Client/ReservationCreationViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tourismo.Core.Commands.Client;
+using Tourismo.Core.Model.TravelManagement;
+using Tourismo.Core.Utility;
+using Tourismo.GUI.Utility;
+
+namespace Tourismo.GUI.Client
+{
+    public class ReservationCreationViewModel : NavigableViewModel
+    {
+
+        private Travel _travel;
+        public Travel Travel
+        {
+            get => _travel;
+            set
+            {
+                _travel = value;
+                OnPropertyChanged(nameof(Travel));
+            }
+        }
+
+
+        public ReservationCreationViewModel()
+        {
+            _travel = GlobalStore.ReadObject<Travel>("TravelForReservation");
+        }
+    }
+}
